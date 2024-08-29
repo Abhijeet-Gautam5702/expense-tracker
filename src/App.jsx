8784ceb9243f98482790c1a90773fb65d0f29701
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-08-29 16:25:29 +0530

    Add IIFE to populate store with user-auth-data upon render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Header, Footer, Container } from "./components";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login as storeLogin } from "./store/authSlice/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  // Whenever App is loaded => populate store.auth if the user is logged in
+  useEffect(() => {
+    (async () => {
+      try {
+        const userData = await authService.getLoggedInUser();
+        if (userData) {
+          dispatch(storeLogin({ userData }));
+        }
+      } catch (error) {
+        console.log("Error in App.jsx useEffect: ", error.message);
+      }
+    })();
+    // console.log("App.jsx loaded");
+  }, []);
   return (
     <div className="w-full min-h-screen flex flex-col justify-between items-center">
       <Header />
